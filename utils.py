d37f6ba1a63572459bd87d862b683d82491c8df1
skulsik <skulsik1@gmail.com>
2023-02-07 18:02:09 +0700

    add code in project(modified)

--- utils.py
+++ utils.py
@@ -2,6 +2,46 @@ import requests, datetime
 
 
 class ListOfOperation:
+    def modified_from_account(self, operation):
+        """
+        Скрываем часть счета отправителя, разбиваем счет по 4 цифры
+        :param operation: Передает строку с названием счета и сам счет, может быть пустая строка
+        :return: Возврат частично скрытых символов в счете
+        """
+        # Проверка существования счета отправителя
+        if 'from' in operation:
+
+            # Разбиваем строку по пробелам, получаем: слово/слова и последний элемент номер счета
+            from_account = operation['from'].split()
+            # Выделяем номер счет в переменную
+            account = from_account[len(from_account) - 1]
+            # Удаляем номер счета из переменной, для дальнейшего склеивания с модифицированной переменной
+            del from_account[len(from_account) - 1]
+
+            # Разбиваем номер счета на список из четырех символов и тут же склеиваем в строку
+            modified_account = ' '.join(account[i:i + 4] for i in range(0, len(account), 4))
+            # На случай когда символов в счете > 16
+            if len(modified_account) > 19:
+                add_str = "**** "
+            else:
+                add_str = ""
+            modified_account = " " + modified_account[0:7] + "** **** " + add_str + modified_account[len(modified_account) - 4:len(modified_account)]
+
+            from_account = " ".join(from_account) + modified_account
+            return from_account
+        else:
+            return "Номер счета неизвестен"
+
+    def modified_to_account(self, operation):
+        """
+        Скрываем часть счета получателя
+        :param operation: Передает строку с названием счета и сам счет
+        :return: Возврат частично скрытых символов в счете
+        """
+        to_account = operation[0:4] + " **" + operation[21:25]
+        return to_account
+
+
     def __init__(self):
         self.list_of_operations = []
         self.list_of_date = []
@@ -66,32 +106,11 @@ class ListOfOperation:
                     # Выводим данные по ТЗ
                     print(f'{date_D.strftime("%d.%m.%Y")} {operation["description"]}')
 
-                    # Проверка существования счета отправителя
-                    if 'from' in operation:
+                    # Скрываем часть счета отправителя
+                    from_account = self.modified_from_account(operation)
 
-                        # Разбиваем строку по пробелам, получаем: слово/слова и последний элемент номер счета
-                        from_account = operation['from'].split()
-                        # Выделяем номер счет в переменную
-                        account = from_account[len(from_account)-1]
-                        # Удаляем номер счета из переменной, для дальнейшего склеивания с модифицированной переменной
-                        del from_account[len(from_account)-1]
-
-                        # Разбиваем номер счета на список из четырех символов и тут же склеиваем в строку
-                        modified_account = ' '.join(account[i:i + 4] for i in range(0, len(account), 4))
-                        # На случай когда символов в счете > 16
-                        if len(modified_account) > 19:
-                            add_str = "**** "
-                        else:
-                            add_str = ""
-                        modified_account = " " + modified_account[0:7] + "** **** " + add_str + modified_account[len(modified_account)-4:len(modified_account)]
-
-                        from_account = " ".join(from_account) + modified_account
-                    else:
-                        from_account = "Номер счета неизвестен"
-
-                    # Скрываем счет на который поступили средства
-                    to_account = operation['to']
-                    to_account = to_account[0:4] + " **" + to_account[21:25]
+                    # Скрываем часть счета на который поступили средства
+                    to_account = self.modified_to_account(operation['to'])
 
                     print(f"{from_account} -> {to_account}")
                     print(f"{operation['operationAmount']['amount']} {operation['operationAmount']['currency']['name']}\n")
